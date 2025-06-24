1f99777f2c14fe6885ee8a9fffb14190ba3e9b53
Filip Doležal <fidokadan@gmail.com>
2025-06-24 19:43:52 +0200

    Added yazi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,15 @@ starship init fish | source
 
 ~/scripts/random_cow_fortune.sh
 
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
